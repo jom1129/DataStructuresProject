0bdfa613fe8ba8304753f0fced3f7465a6ab1247
Kurt Matthew <kurtmtthw@gmail.com>
2021-08-26 22:09:54 +0800

    Added comments

--- src/Timer.java
+++ src/Timer.java
@@ -1,8 +1,6 @@
-
 // Code adapted From R. Sedgewick's Algorithms 4th ed.
 // Book homepage: https://algs4.cs.princeton.edu/home/
-
-public class Timer {
+public final class Timer {  // Do not extend the timer class
     /*
         Usage:
             1. Instantiate. Upon instantiation the current system time
@@ -11,10 +9,26 @@ public class Timer {
                timeElapsed(). // timer.timeElapsed()
      */
 
+
     private long startTime; // holds the beginning time
+
+    /**
+     * Records the current system time upon instantiation.
+     * This is why it is necessary that the instantiation procedure
+     * Be the FIRST** statement in a sorting algorithm
+     */
     public Timer() { startTime = System.currentTimeMillis(); }  // Assigns the current time
                                                                 // to startTime
                                                                 // upon instantiation
+
+    /**
+     * Subtracts the beginning time from the current system time
+     * to compute for the elapsed time
+     * division by 1000.00 is necessary because time is represented
+     * using the long data type
+     * @return the elapsed time
+     */
+
     public double timeElapsed() {
         long currentTime = System.currentTimeMillis(); // Re-invocation of the method
         return (currentTime - startTime) / 1000.0;     // returns the updated time
